3bcfd557ef337cab9999a0d71916a345a5d24c5d
Valdir Coxev <valdircoxevdev@gmail.com>
2019-06-20 14:55:39 -0400

    Adding page login

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignIn = () => <h1>Login</h1>;
+import { Container, LogoContainer, SignForm } from './styles';
+
+import Logo from '~/assets/logo.svg';
+
+class SignIn extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Container>
+        <LogoContainer src={Logo} alt="logo" />
+        <SignForm>
+          <input
+            type="email"
+            name="email"
+            autoComplete="off"
+            placeholder="Seu e-mail"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Senha secreta"
+            value={password}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Entrar</button>
+        </SignForm>
+      </Container>
+    );
+  }
+}
 
 export default SignIn;
